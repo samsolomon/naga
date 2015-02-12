9133f612c2da450cb9981c3bb595f21bf3a0f8b0
Sam Solomon <sam@solomon.io>
2015-02-12 10:19:31 -0600

    first commit

---
+++ footer.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package NAGA
+ */
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'naga' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'naga' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( __( 'Theme: %1$s by %2$s.', 'naga' ), 'NAGA', '<a href="http://solomon.io/" rel="designer">Sam Solomon</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
