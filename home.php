1bd96622474c9bcee4e763df894994a0e787dfbc
Sam Solomon <sam@solomon.io>
2015-03-09 11:17:12 -0500

    homepage custom fields

--- home.php
+++ home.php
@@ -3,3 +3,13 @@
 Template Name: Home
 */
 ?>
+
+<?php get_header(); ?>
+
+<h1><?php the_field('hero_text'); ?></h1>
+
+<p><?php the_field('subhead'); ?></p>
+
+<a href="<?php the_field('button_link'); ?>"><?php the_field('button_text'); ?></a>
+
+<?php get_footer(); ?>
