01dbf4f046e6f1ee76429da38e6eeb76e6fce2d8
Sam Solomon <sam@solomon.io>
2015-03-19 14:32:02 -0500

    afc fields added to about page

--- content-page.php
+++ content-page.php
@@ -30,6 +30,52 @@
 		?>
 	</div><!-- .entry-content -->
 
+	<!-- ABOUT PAGE  -->
+	<?php
+	// OFFICERS
+	// check if the repeater field has rows of data
+	if( have_rows('officers') ):
+	 	// loop through the rows of data
+	    while ( have_rows('officers') ) : the_row();
+	        // display a sub field value
+	        the_sub_field('officer_name');
+					the_sub_field('officer_title');
+					the_sub_field('officer_image');
+	    endwhile;
+	else :
+	    // no rows found
+	endif;
+
+	//TRUSTEES
+	// check if the repeater field has rows of data
+	if( have_rows('trustees') ):
+		// loop through the rows of data
+			while ( have_rows('trustees') ) : the_row();
+					// display a sub field value
+					the_sub_field('trustee_name');
+					the_sub_field('trustee_title');
+					the_sub_field('trustee_image');
+			endwhile;
+	else :
+			// no rows found
+	endif;
+
+	//REGIONAL ASSOCIATIONS
+	// check if the repeater field has rows of data
+	if( have_rows('regional_associations') ):
+		// loop through the rows of data
+			while ( have_rows('regional_associations') ) : the_row();
+					// display a sub field value
+					the_sub_field('regional_image');
+					the_sub_field('regional_information');
+			endwhile;
+	else :
+			// no rows found
+	endif;
+	?>
+
+
+
 	<footer class="entry-footer">
 		<?php edit_post_link( __( 'Edit', 'naga' ), '<span class="edit-link">', '</span>' ); ?>
 	</footer><!-- .entry-footer -->
