8cf65d2bfe5a574b7fdefc18836ffe451af95579
Sam Solomon <sam@solomon.io>
2015-03-20 09:34:17 -0500

    finally figured out the repeater

--- content-page.php
+++ content-page.php
@@ -37,45 +37,40 @@
 	if( have_rows('officers') ):
 	 	// loop through the rows of data
 	    while ( have_rows('officers') ) : the_row();
-					//variables
-					$name = get_sub_field('name');
-					$title = get_sub_field('title');
-					$image = get_sub_field('image');
-					?>
+					//vars
+					$officer_name = get_sub_field('officer_name');
+					$officer_title = get_sub_field('officer_title');
+					$officer_image = get_sub_field('officer_image');				?>
 
-					<?php if( $name ): ?>
-						<p><?php echo $name; ?></p>
+					<?php if( $officer_name ): ?>
+						<p><?php echo $officer_name; ?></p>
 					<?php endif; ?>
 
-					<?php if( $title ): ?>
-					<p><?php echo $title; ?></p>
+					<?php if( $officer_title ): ?>
+						<p><?php echo $officer_title; ?></p>
 					<?php endif; ?>
 
-					<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt'] ?>" />
-
+					<img src="<?php echo $officer_image['url']; ?>" alt="<?php echo $officer_image['alt'] ?>" />
 			<?php
-			endwhile;
+	    endwhile;
 	else :
 	    // no rows found
 	endif;
-?>
-<?php
+
 	//TRUSTEES
 	// check if the repeater field has rows of data
 	if( have_rows('trustees') ):
 		// loop through the rows of data
-			while ( have_rows('trustees') ) : the_row(); ?>
-					<p><?php the_sub_field('trustee_name');?></p>
-					<p><?php the_sub_field('trustee_title');?></p>
-					<img src="<?php trustee_image('image'); ?>" alt="<?php trustee_image('alt'); ?>" />
-			<?php
+			while ( have_rows('trustees') ) : the_row();
+					// display a sub field value
+					the_sub_field('trustee_name');
+					the_sub_field('trustee_title');
+					the_sub_field('trustee_image');
 			endwhile;
 	else :
 			// no rows found
 	endif;
-?>
 
-<?php
 	//REGIONAL ASSOCIATIONS
 	// check if the repeater field has rows of data
 	if( have_rows('regional_associations') ):
